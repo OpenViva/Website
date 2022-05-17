e7574c16a4d1226e8b0fbe8a80b18952f74714b4
Fun Maker <funmaker95@gmail.com>
2022-05-17 02:25:01 +0200

    Favicon, OpenGraph and Privacy Policy

--- client/routes/index/IndexPage.tsx
+++ client/routes/index/IndexPage.tsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import { IndexPageResponse } from "../../../types/api";
 import Segment from "../../components/Segment";
 import Button from "../../components/Button";
-import usePageData from "../../hooks/usePageData";
+import usePageData, { useConfig } from "../../hooks/usePageData";
 import Section from "../../components/Section";
 import ExLink from "../../components/ExLink";
 import Paragraph from "../../components/Paragraph";
 import "./IndexPage.scss";
 
 export default function IndexPage() {
+  const config = useConfig();
   const [pageData] = usePageData<IndexPageResponse>();
   
   let buttons: React.ReactNode;
@@ -28,13 +29,18 @@ export default function IndexPage() {
         <img className="logo" src="/static/logoBig.png" alt="OpenViva" />
         <Segment className="main">
           <Paragraph header="About">
-            We are a group of users dedicated to continuing viva project as a free and open source game under the name "OpenViva".
-            Our goal is to polish 0.8.03 and replace any and all third party paid assets with either free or custom assets.
-            All assets and scripts will be available to anyone who wants to download them or fork the project on our github.
+            OpenViva is an free and open source continuation of Project Viva, originally made by Sir Hal. Viva is a VR
+            and non-VR compatible game where you can interact with your very own AI anime character! It is an advanced AI
+            simulation that can interact and respond to many of your actions in a dynamic way through inverse kinematics,
+            complex behaviors, and currently over 200 animations. The character mood is dynamic and responds to how you treat it.
+            You can play with it, feed it, or just be friends with it. You can download and play OpenViva for free using buttons below.
           </Paragraph>
-          <Paragraph header="Plans">
-            Our plans are to clean up and improve the AI that runs the characters, add more extensive character support such as .pmx parsing,
-            quest 2 support, steam releases, steam achievements and a proper modloader that works with the steam workshop.
+          <Paragraph header="Team">
+            We are a group of users dedicated to continuing viva project as a free and open source game under the name &quot;OpenViva&quot;.
+            Our goal is to polish 0.8.03 and replace any and all third party paid assets with either free or custom assets.
+            All assets and scripts are be available to anyone who wants to download them or fork the project on our
+            {" "}<ExLink to="https://github.com/OpenViva">GitHub</ExLink>. If you have any questions or want to help us, you can join our
+            community on <ExLink to={config.discordInvite} className="discord">Discord</ExLink>.
           </Paragraph>
           <div className="buttons">{buttons}</div>
         </Segment>
--- client/routes/privacy/PrivacyPage.tsx
+++ client/routes/privacy/PrivacyPage.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import Section from "../../components/Section";
 import Segment from "../../components/Segment";
 import Paragraph from "../../components/Paragraph";
+import { useConfig } from "../../hooks/usePageData";
+import EmailLink from "../../components/EmailLink";
 
 export default function PrivacyPage() {
+  const config = useConfig();
+  
   return (
     <div className="FaqPage">
       <Section>
         <Segment>
           <Paragraph header="Privacy policy">
-            Viva-project.org is a publicly accessible website and therefore all content uploaded to the site will first be verified before publishing.
-            
-            When not logged in, your data will not be collected. After account creation, your email address is collected along with records of your uploads to the site for takedown notices.
-            
-            We only use cookies for essential functions of the site and are only stored when you are logged in or sign up.
+            By accessing this site or creating an account, you agree to our privacy policy.<br />
+            Viva-project.org is a publicly accessible website and therefore all content uploaded to the site will first be verified before publishing.<br />
+            When you login/register, the only information we collect is your email address and ip address. When not logged in, no data is collected.<br />
+            We use cookies only for essential site functionality.<br />
+            All of the cards hosted on this website are user created. If you would like to request a takedown/dmca for copyright infringement, you can contact us at <EmailLink address={config.contactEmailBase64} /> and we will remove it.<br />
+            If you would like additional information pertaining to the privacy policy or to delete your account and it&apos;s associated information, you may contact us via our email and additional clarification can be provided.
           </Paragraph>
         </Segment>
       </Section>
